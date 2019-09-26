7e42c307d6c442f45fb971548d57918afdf4088b
elecshan <xgs951230@163.com>
2019-09-26 16:04:27 +0800

    Part 2

---
+++ build/CMakeFiles/db.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xgs/Scripts/C/Database/command.cpp" "/home/xgs/Scripts/C/Database/build/CMakeFiles/db.dir/command.cpp.o"
+  "/home/xgs/Scripts/C/Database/main.cpp" "/home/xgs/Scripts/C/Database/build/CMakeFiles/db.dir/main.cpp.o"
+  "/home/xgs/Scripts/C/Database/sds.cpp" "/home/xgs/Scripts/C/Database/build/CMakeFiles/db.dir/sds.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/db.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/db.dir/command.cpp.o"
+  "CMakeFiles/db.dir/main.cpp.o"
+  "CMakeFiles/db.dir/sds.cpp.o"
+  "../bin/db.pdb"
+  "../bin/db"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/db.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
